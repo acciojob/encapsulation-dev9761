d722ab9e266aa198614cedec1cfc47e1193a5f2b
Devansh <saxenad276@gmail.com>
2023-07-22 16:29:58 +0530

    update

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,7 +3,7 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         RWOnly rw = new RWOnly();
-        rw.name = "dev"; //Error: The variable "name" is not visible
+        //rw.name = "dev"; //Error: The variable "name" is not visible
         rw.setName("dev");
         System.out.println(rw.getName());
     }
